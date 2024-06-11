026c09eeecf19480ebbf539a4fc6c8f63fb25b4b
Wyat Soule <wsoule679@gmail.com>
2024-06-10 20:33:22 -0400

    more bufferline settings

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -2,4 +2,15 @@ return {
   'akinsho/bufferline.nvim',
   version = '*',
   dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function()
+    require('bufferline').setup {
+      options = {
+        diagnostics = 'nvim_lsp',
+      },
+    }
+
+    vim.keymap.set('n', '<C-S-[>', ':bprevious<CR>', { silent = true })
+    vim.keymap.set('n', '<C-S-]>', ':bNext<CR>', { silent = true })
+    vim.keymap.set('n', '<C-S-T>', ':e #<CR>', { silent = true })
+  end,
 }
