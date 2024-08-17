91c7142cfbe2252889e24c8da4693bca33b9b155
Wyat Soule <wsoule679@gmail.com>
2024-08-17 08:24:16 -0400

    update keymap

--- lua/custom/keymap.lua
+++ lua/custom/keymap.lua
@@ -1,33 +1,31 @@
 local modes = { 'n', 'i', 'v' }
 
--- Open file view (bigger neotree).
+-- Open file view (bigger neotree)
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
--- Move selected text - ThePrimeagen.
--- vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
--- vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
--- Keep curor where it was when doing J - ThePrimeagen.
+
+-- Keep cursor where it was when doing J
 vim.keymap.set('n', 'J', 'mzJ`z')
--- Move up or down 50 lines - ThePrimeagen.
+
+-- Move up or down 50 lines
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 
--- Search work under cursor.
+-- Search word under cursor
 vim.keymap.set(
   'n',
   '<leader>S',
   ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gIc<Left><Left><Left><Left>'
 )
 
--- Keep search terms in the middle of the screen - ThePrimeagen.
+-- Keep search terms in the middle of the screen
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
 
--- Keep p reserved when pasting over something - ThePrimeagen.
+-- Keep p reserved when pasting over something
 vim.keymap.set('x', '<leader>p', '"_dP')
 
--- Yank things into clipboard - asbjornHaland.
+-- Yank to clipboard
 vim.keymap.set('n', '<leader>y', '"+y', { desc = 'Copy into clipboard.' })
-
 vim.keymap.set(
   'v',
   '<leader>y',
@@ -41,6 +39,7 @@ vim.keymap.set(
   { desc = '[Y]ank line into clipboard.' }
 )
 
+-- Delete to black hole register
 vim.keymap.set(
   'n',
   '<leader>d',
@@ -54,26 +53,23 @@ vim.keymap.set(
   { desc = 'Copy highlighted deleted text into clipboard' }
 )
 
+-- Exit insert mode with Ctrl-C
 vim.keymap.set('i', '<C-c>', '<Esc>')
--- Yank whole file to clipboard.
+
+-- Yank whole file to clipboard
 vim.keymap.set('n', '<C-c>', ':%y+<CR>', { silent = true })
 
+-- Disable Q
 vim.keymap.set('n', 'Q', '<nop>')
--- vim.keymap.set('n', '<C-f>', '<cmd>silent !tmux neww tmux-sessionizer<CR>')
 
--- go to errors
--- these might not even work
--- vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
--- vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
+-- Go to errors
 vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
 vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
 
-vim.keymap.set('n', '<leader>x', '<cmd>!chmod +c %<CR>', { silent = true })
+-- Change file permissions
+vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
 
--- [[ Basic Keymaps ]]
---  See `:help vim.keymap.set()`
-
--- Set highlight on search, but clear on pressing <Esc> in normal mode
+-- Set highlight on search, clear on <Esc>
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
@@ -103,12 +99,7 @@ vim.keymap.set(
   { desc = 'Open diagnostic [Q]uickfix list' }
 )
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
+-- Exit terminal mode
 vim.keymap.set(
   't',
   '<Esc><Esc>',
@@ -116,16 +107,13 @@ vim.keymap.set(
   { desc = 'Exit terminal mode' }
 )
 
--- TIP: Disable arrow keys in normal mode
+-- Disable arrow keys in normal mode
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
+-- Split navigation
 vim.keymap.set(
   'n',
   '<C-h>',
@@ -151,13 +139,8 @@ vim.keymap.set(
   { desc = 'Move focus to the upper window' }
 )
 
--- Open command with ;
--- commenting this out becuase to repeat the 'f' you need to use ';'
--- vim.keymap.set('n', ';', ':', { desc = 'Open command.' })
-
--- For keymaps that affect every mode.
+-- Save file in all modes
 for _, mode in ipairs(modes) do
-  -- Save file in all modes
   vim.keymap.set(
     mode,
     '<C-s>',
@@ -166,10 +149,19 @@ for _, mode in ipairs(modes) do
   )
 end
 
+-- Yank entire buffer
 vim.keymap.set('n', '<C-S-A>', 'ggVG')
 
--- These mappings control the size of splits (height/width)
+-- Control split size
 vim.keymap.set('n', '<M-,>', '<c-w>5<')
 vim.keymap.set('n', '<M-.>', '<c-w>5>')
 vim.keymap.set('n', '<M-t>', '<C-W>+')
 vim.keymap.set('n', '<M-s>', '<C-W>-')
+
+-- Close buffer without closing the split
+vim.keymap.set(
+  'n',
+  '<leader>bd',
+  ':bp|bd #<CR>',
+  { desc = 'Close buffer without closing split' }
+)
